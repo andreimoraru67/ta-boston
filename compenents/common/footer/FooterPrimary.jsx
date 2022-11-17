7c3dba8d2911ffa7892b68b56fea82e28b523e28
andreimoraru67 <andreimoraru3496@gmail.com>
2022-11-17 21:30:56 +0200

    TA

---
+++ compenents/common/footer/FooterPrimary.jsx
@@ -0,0 +1,121 @@
+import Link from 'next/link';
+
+export default function FooterPrimary() {
+  return (
+    <div className="container footer-primary-inner">
+      <nav className="footer-nav">
+        <ul>
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+
+          <li>
+            <Link href="/" title="Informatii"></Link>Informatii
+          </li>
+        </ul>
+      </nav>
+
+      <section className="footer-contact">Add and style contact</section>
+
+      <nav className="footer-support">
+        <ul>
+          <li>
+            <a href="tel:+0712345678" title="Call us">
+              <i className="fa-solid fa-phone"></i>Call us
+            </a>
+          </li>
+
+          <li>
+            <a href="tel:+0712345678" title="Call us">
+              <i className="fa-solid fa-phone"></i>Call us
+            </a>
+          </li>
+
+          <li>
+            <a href="tel:+0712345678" title="Call us">
+              <i className="fa-solid fa-phone"></i>Call us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,65 @@
+import Link from 'next/link';
+import Footer from '../compenents/common/footer/Footer';
+import Header from '../compenents/common/header/Header';
+import BsCatalogPreview from '../compenents/homepage/BsCatalogPreview';
+import HomeCatalogPreview from '../compenents/homepage/HomeCatalogPreview';
+import HomeNewsletter from '../compenents/homepage/HomeNewsletter';
+import HomePayment from '../compenents/homepage/HomePayment';
+
 export default function Home() {
-  return (<div>hello boston
-    <i className="fa-solid fa-house"></i>
+  return (
+    <div>
+      <header className="header container mt-lg-5 mb-lg-3">
+        <Header></Header>
+      </header>
 
-<img src="/images/logo.jpg" alt="Alt" />
+      <main className="content">
+        <header className="banner-main">
+          <div className="banner-main-inner container">
+            <div>
+              <h1>Office Suits</h1>
+              <h2>Get back to work in style</h2>
 
-<button className="button button-primary">Button Primary</button>
-<button className="button button-primary button-large">Button Primary Large</button>
+              <Link
+                href="/"
+                title="CTA"
+                className="banner-cta button button-large"
+              >
+                Create your suit
+              </Link>
+            </div>
+            <footer className="banner-main-copy">
+              <p>
+                <span>A Tailored Suit</span> that will fit you perfectly is a
+                dream for everybody.
+              </p>
+              <p>
+                We provide you with the design and you choose if you like it or
+                if you want to change the details with our online configurator.
+              </p>
+            </footer>
+          </div>
+        </header>
 
+        <div className="content-main">
+          <HomeCatalogPreview></HomeCatalogPreview>
 
-<a className="button button-primary">Button Primary</a>
-<a className="button button-primary button-large">Button Primary Large</a>
-  </div>
+          <HomeCatalogPreview></HomeCatalogPreview>
 
+          <HomeCatalogPreview></HomeCatalogPreview>
+
+          {/* <HomeCatalogPreview></HomeCatalogPreview> */}
+
+          <BsCatalogPreview></BsCatalogPreview>
+
+          <HomePayment></HomePayment>
+          <HomeNewsletter></HomeNewsletter>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <Footer></Footer>
+      </footer>
+    </div>
   );
 }
-
